180076f55cdb241aa13a4f4051ac864b505f670f
UmangLadha <umangladha02@gmail.com>
2021-08-24 18:41:09 +0530

    mock server add

--- app.js
+++ app.js
@@ -1,13 +1,17 @@
 var btntranslate = document.querySelector("#btn-translate");
-var inputtext = document.querySelector("#text-input");
-var outputdiv = document.querySelector("#output-translate");
-
+var inputtext = document.querySelector("#input-txt");
+var outputtext = document.querySelector("#output-txt");
+var url= "https://api.funtranslations.com/translate/minion.json";
+function getURL(text){
+    return url + "?text=" + text;
+}
 
  function clickhandler() 
  {
-    outputdiv.innertext = "banana language"+ inputtext.value;
-}
-console.log(outputdiv)
-
-btntranslate.addEventListener("click" , clickhandler);
+    var text = inputtext.value;
+    fetch(getURL(text))
+    .then(response => response.json())
+    .then(json => outputtext.innerText = json.contents.translated );
+ };
+ btntranslate.addEventListener("click" , clickhandler);
   
